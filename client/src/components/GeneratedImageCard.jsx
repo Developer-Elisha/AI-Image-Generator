c5dbd5ca56963de76f9db36a1e22d0067532ae69
Developer-Elisha <developer.elisha@gmail.com>
2024-12-26 21:35:19 +0500

    Frontend is Completed with two pages 1. Create Post, 2. Explore Community PageNo:1. Creat Post: It has a navigation bar then two section on row formate 1. It has one input for Author Name and textarea for Prompt of Image then Generate & Post Image Buttons & on the right hand side it has preview of the Generated Image PageNo:2. Explore Community: It also have the same navigation bar the a Search bar for Image by  with a search icon then Images in grid view of

---
+++ client/src/components/GeneratedImageCard.jsx
@@ -0,0 +1,50 @@
+import { CircularProgress } from '@mui/material';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  flex: 1;
+  min-height: 300px;
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  border: 2px dashed ${({ theme }) => theme.yellow};
+  color: ${({ theme }) => theme.arrow + 80};
+  border-radius: 20px;
+`;
+
+const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 24px;
+  background: ${({ theme, src }) => theme.black + 50};
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
+`;
+
+const GeneratedImageCard = ({ src, loading }) => {
+  return (
+    <Container>
+        {loading ? (
+          <>
+            <CircularProgress style={{ color: "inherit", width: "24px", height: "24px" }} />
+            Generating Your Image ...</>
+        ) : (
+          <>
+            {src ? (
+              <Image src={src} />
+            ) : (
+              <>Write a prompt to generate an image</>
+            )}
+          </>
+        )}
+    </Container>
+  );
+};
+
+export default GeneratedImageCard;
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,15 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import GenerateImageForm from '../components/GenerateImageForm';
+import GeneratedImageCard from '../components/GeneratedImageCard';
 
 const Container = styled.div`
-height: 100%;
-overflow-y: scroll;
+  height: 100vh;
+  overflow-y: auto;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  max-width: 1200px;
+  gap: 8%;
+  display: flex;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const CreatePost = () => {
+  const [generateImageLoading, setGenerateImageLoading] = useState(false);
+  const [createPostLoading, setcreatePostLoading] = useState(false);
+  const [post, setPost] = useState({
+    author: "",
+    prompt: "",
+    photo: "",
+  });
   return (
-    <Container>CreatePost</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <GenerateImageForm
+          post={post}
+          setPost={setPost}
+          createPostLoading={createPostLoading}
+          setGenerateImageLoading={setGenerateImageLoading}
+          generateImageLoading={generateImageLoading}
+          setcreatePostLoading={setcreatePostLoading}
+        />
+        <GeneratedImageCard
+          src={post?.photo}
+          loading={generateImageLoading}
+        />
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
