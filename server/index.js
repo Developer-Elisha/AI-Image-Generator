ca641b321ee04e875ed56d0bf956b595f2cd02f5
Developer-Elisha <developer.elisha@gmail.com>
2025-01-22 03:32:47 +0500

    Fix backend deployment issues

--- server/index.js
+++ server/index.js
@@ -12,10 +12,12 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true}));
 
+app.use(cors());
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
 }));
+
 
 
 // error handler
@@ -55,11 +57,12 @@ const PORT = process.env.PORT || 8080;
 // function to start the server
 const startServer = async () => {
     try {
-        connectDB();
-        app.listen(PORT, () => console.log("Server started on port 8080"));
-    }catch(error) {
+        await connectDB(); // Ensure DB is connected before starting the server
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
         console.log(error);
     }
 };
+
 
 startServer();
