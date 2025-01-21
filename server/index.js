8828935346661faf73d48fb8ff88294a651ab225
Developer-Elisha <developer.elisha@gmail.com>
2025-01-22 02:38:44 +0500

    vercel.json

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true}));
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+
 // error handler
 app.use((err, req, res, next) => {
     const status = err.status || 500;
@@ -44,11 +50,13 @@ const connectDB = () => {
     });
 };
 
+const PORT = process.env.PORT || 8080;
+
 // function to start the server
 const startServer = async () => {
     try {
         connectDB();
-        app.listen(8080, () => console.log("Server started on port 8080"));
+        app.listen(PORT, () => console.log("Server started on port 8080"));
     }catch(error) {
         console.log(error);
     }
